b0c40f3cc1fa2551815fe3b7fc0b3fb7833f1445
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-24 16:21:45 +0530

    Added proxy for auth service form gateway

--- gateway/index.js
+++ gateway/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import proxy from 'express-http-proxy';
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use("/auth", proxy(process.env.AUTH_SERVICE));
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: 'Gateway request success.' });
