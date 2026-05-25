e56bb036154201799eafb22031f3d59c311423c6
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-25 17:22:34 +0530

    Allowed cors origins and added cookie parser in gateway

--- gateway/index.js
+++ gateway/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import proxy from 'express-http-proxy';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cookieParser());
 app.use("/auth", proxy(process.env.AUTH_SERVICE));
 
 app.get('/', (req, res) => {
