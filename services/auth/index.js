27a5972bf6c5e39a9cfdea875aeae501568c5097
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-25 15:14:26 +0530

    Connected firebase to auth service

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import connectDB from './config/db.js';
 
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const PORT = process.env.PORT;
 
 const app = express();
