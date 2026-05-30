eb07524f0a4b3db63cafb32eda11a1e3ce25b97d
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-30 16:56:15 +0530

    User id is passed as header to chat service

--- gateway/index.js
+++ gateway/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { getCurrentUser } from './controllers/user.controller.js';
 import protect from './middleware/auth.middleware.js';
+import { proxyWithHeader } from './utils/proxyWithHeader.js';
 
 const PORT = process.env.PORT;
 
@@ -14,7 +15,7 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", proxy(process.env.AUTH_SERVICE));
-app.use("/api/chat", proxy(process.env.CHAT_SERVICE));
+app.use("/api/chat", protect, proxyWithHeader(process.env.CHAT_SERVICE));
 
 app.get("/api/me", protect, getCurrentUser)
 
