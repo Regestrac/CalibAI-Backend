9117365a5bf13655a1bf06e4cc19dd6ec334c024
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-30 15:41:01 +0530

    Connected chat service with proxy from gateway

--- gateway/index.js
+++ gateway/index.js
@@ -14,6 +14,7 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", proxy(process.env.AUTH_SERVICE));
+app.use("/api/chat", proxy(process.env.CHAT_SERVICE));
 
 app.get("/api/me", protect, getCurrentUser)
 
