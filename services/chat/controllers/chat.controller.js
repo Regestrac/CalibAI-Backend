ce1b869c75dc11524ff23154ffcd16ad608c3da9
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-31 16:29:11 +0530

    Added get messages controller

--- services/chat/controllers/chat.controller.js
+++ services/chat/controllers/chat.controller.js
@@ -57,3 +57,15 @@ export const saveMessage = async (req, res) => {
     return res.status(500).json({ message: `Save message error: ${error}` });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const messages = Message.find({
+      conversationId: req.params.conversationId,
+    }).sort({ createdAt: -1 });
+
+    return res.status(200).json(messages);
+  } catch (error) {
+    return res.status(500).json({ message: `Get messages error: ${error}` });
+  }
+};
