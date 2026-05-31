564fca03d6ab31507a9b5024f2cadc48295cb658
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-31 17:16:13 +0530

    reated routes for chat related controllers

--- services/chat/index.js
+++ services/chat/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import dns from 'dns';
+import router from './routes/chat.routes.js';
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(express.json());
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from Chat" });
