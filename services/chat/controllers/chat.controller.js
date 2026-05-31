2846db3d343197e46c7f603281b370e7cafc132f
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-31 14:21:37 +0530

    Created controller for creating conversation

---
+++ services/chat/controllers/chat.controller.js
@@ -0,0 +1,16 @@
+import Conversation from "../models/conversation.model.js";
+
+export const createConversation = async (req, res) => {
+  try {
+    const userId = req.headers["x-user-id"];
+    console.log('userId: ', userId);
+
+    const conversation = await Conversation.create({
+      userId,
+    });
+
+    return res.status(200).json(conversation);
+  } catch (error) {
+    return res.status(500).json({ message: `Create conversation error: ${error}` });
+  }
+};
