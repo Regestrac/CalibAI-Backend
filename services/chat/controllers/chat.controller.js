fc378d92c3a8678598b4c49c1ca5f928203f7431
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-31 14:43:21 +0530

    Added get conversations controller

--- services/chat/controllers/chat.controller.js
+++ services/chat/controllers/chat.controller.js
@@ -14,3 +14,17 @@ export const createConversation = async (req, res) => {
     return res.status(500).json({ message: `Create conversation error: ${error}` });
   }
 };
+
+export const getConversations = async (req, res) => {
+  try {
+    const userId = req.headers["x-user-id"];
+
+    const conversations = await Conversation.find({
+      userId,
+    }).sort({ updatedAt: -1 });
+
+    return res.status(200).json(conversations);
+  } catch (error) {
+    return res.status(500).json({ message: `Get conversations error: ${error}` });
+  }
+};
