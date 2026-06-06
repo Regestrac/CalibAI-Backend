c4d00f2c3c4de3b993ccb3582f223dcae69c4143
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-06 14:33:16 +0530

    Configured connecting to selected agent with conditional edge

--- services/agent/graph/graph.js
+++ services/agent/graph/graph.js
@@ -1,15 +1,36 @@
 import { StateGraph } from "@langchain/langgraph";
 import { agentState } from "./state";
 import { router } from "./router";
+import { chatAgent } from "../agents/chat.agent";
+import { codingAgent } from "../agents/coding.agent";
+import { searchAgent } from "../agents/search.agent";
+import { imageAgent } from "../agents/image.agent";
+import { pdfAgent } from "../agents/pdf.agent";
+import { pptAgent } from "../agents/ppt.agent";
 
 const workflow = StateGraph(agentState);
 
 workflow.addNode("router", router);
 workflow.addNode("chat", chatAgent);
+workflow.addNode("coding", codingAgent);
 workflow.addNode("search", searchAgent);
-workflow.addNode("imageGen", imageGenAgent);
+workflow.addNode("image", imageAgent);
 workflow.addNode("pdf", pdfAgent);
 workflow.addNode("ppt", pptAgent);
 
 workflow.addEdge("__start__", "router");
-workflow.addConditionalEdges("router", router);
+
+workflow.addConditionalEdges(
+  "router",
+  (state) => {
+    return state.agent || "chat";
+  },
+  {
+    chat: "chat",
+    coding: "coding",
+    search: "search",
+    image: "image",
+    pdf: "pdf",
+    ppt: "ppt",
+  },
+);
