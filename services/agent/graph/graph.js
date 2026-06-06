f755c5bea4e3925116981936c04597a408f706a2
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-06 14:48:46 +0530

    Connected all agents to end node and fixed imports

--- services/agent/graph/graph.js
+++ services/agent/graph/graph.js
@@ -1,14 +1,14 @@
 import { StateGraph } from "@langchain/langgraph";
-import { agentState } from "./state";
-import { router } from "./router";
-import { chatAgent } from "../agents/chat.agent";
-import { codingAgent } from "../agents/coding.agent";
-import { searchAgent } from "../agents/search.agent";
-import { imageAgent } from "../agents/image.agent";
-import { pdfAgent } from "../agents/pdf.agent";
-import { pptAgent } from "../agents/ppt.agent";
+import { agentState } from "./state.js";
+import { router } from "./router.js";
+import { chatAgent } from "../agents/chat.agent.js";
+import { codingAgent } from "../agents/coding.agent.js";
+import { searchAgent } from "../agents/search.agent.js";
+import { imageAgent } from "../agents/image.agent.js";
+import { pdfAgent } from "../agents/pdf.agent.js";
+import { pptAgent } from "../agents/ppt.agent.js";
 
-const workflow = StateGraph(agentState);
+const workflow = new StateGraph(agentState);
 
 workflow.addNode("router", router);
 workflow.addNode("chat", chatAgent);
@@ -34,3 +34,12 @@ workflow.addConditionalEdges(
     ppt: "ppt",
   },
 );
+
+workflow.addEdge("search", "chat");
+workflow.addEdge("chat", "__end__");
+workflow.addEdge("coding", "__end__");
+workflow.addEdge("image", "__end__");
+workflow.addEdge("pdf", "__end__");
+workflow.addEdge("ppt", "__end__");
+
+export const graph = workflow.compile();
