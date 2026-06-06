e9565a03bc6a98af456050dca8c13449871eed1c
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-06 16:21:35 +0530

    Implement LLM-based agent router for task-specific request redirection

--- services/agent/graph/router.js
+++ services/agent/graph/router.js
@@ -1,3 +1,43 @@
-export const router = async (params) => {
+import { getModel } from "../config/llmModels"
 
-}
+export const router = async (state) => {
+  const llm = getModel("router");
+
+  const prompt = `
+    You are a router for Calib AI.
+    Use the following prompt to route the user's message to the appropriate agent.
+    
+    Available agents:
+    - chat
+    - search
+    - coding
+    - image
+    - pdf
+    - ppt
+
+    Rules for redirecting:
+    chat:
+      General conversations, explanations, learning, questions, etc.
+    search:
+      Current events, latest information, newa, recent developments, internet lookup, etc.
+    coding:
+      Generating code, debug code, build project, architecture, API design, etc.
+    pdf:
+      Questions about generate PDFs or document context.
+    ppt:
+      Questions about generate PPTs or ppt context.
+    image:
+      Generate image, create image, etc.
+    
+    User Query: ${state.prompt}
+
+    Retun only one word which should be the name of the agent from the available agents list, don't give any extra text or markup or explanation.
+  `
+
+  const response = await llm.invoke(prompt);
+
+  return {
+    ...state,
+    agent: response?.content?.trim()?.toLowerCase(),
+  }
+};
