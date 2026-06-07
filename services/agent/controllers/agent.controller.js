25b3bea99e1142fdb96d295804f13a8e14a24436
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-07 15:12:46 +0530

    Initialized agent microservice with LangGraph-based router and LLM orchestration logic

---
+++ services/agent/controllers/agent.controller.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { graph } from '../graph/graph.js';
+
+export const agent = async (req, res) => {
+  try {
+    const { prompt, conversationId } = req.body;
+
+    await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
+      conversationId,
+      content: prompt,
+      role: "user",
+    });
+
+    const result = await graph.invoke({
+      prompt,
+      conversationId,
+    });
+
+    return res.status(200).json({
+      message: "Agent response",
+      data: result.aiResponse,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: "Agent error: " + error.message,
+    });
+  }
+};
