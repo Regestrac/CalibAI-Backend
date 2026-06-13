ff19ff3a419c112170fd948784670bdaf52bc064
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-13 12:31:50 +0530

    Fixed conversation id of a message not saving to db

--- services/chat/models/message.model.js
+++ services/chat/models/message.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  conversationid: {
+  conversationId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Conversation",
   },
