25b4f372e6b4a46cd0a0de8a5793f0d8932f2385
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-15 16:47:21 +0530

    Implemented search agent with the search tool

--- services/agent/agents/search.agent.js
+++ services/agent/agents/search.agent.js
@@ -1,3 +1,20 @@
-export const searchAgent = async (params) => {
+import { searchTool } from "../config/tavliy.js";
 
-}
+export const searchAgent = async (state) => {
+  try {
+    const results = await searchTool.invoke({ query: state?.prompt });
+
+    return {
+      ...state,
+      searchResults: results,
+      images: results?.images,
+    }
+  } catch (error) {
+    console.log("Search Error: ", error);
+    return {
+      ...state,
+      searchResults: [],
+      images: [],
+    }
+  }
+};
