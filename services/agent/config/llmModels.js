0a53ed4924981d90a72c599a43f9bec170d24651
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-16 14:56:37 +0530

    Setup openrouter with deepseek for coding agent

--- services/agent/config/llmModels.js
+++ services/agent/config/llmModels.js
@@ -1,4 +1,5 @@
 import { ChatGroq } from "@langchain/groq";
+import { ChatOpenRouter } from "@langchain/openrouter";
 
 const gpt = new ChatGroq({
   model: "openai/gpt-oss-120b",
@@ -8,12 +9,20 @@ const compound = new ChatGroq({
   model: "groq/compound",
 });
 
+const openRouter = new ChatOpenRouter({
+  model: "deepseek/deepseek-chat",
+  temperature: 0,
+  maxTokens: 2500,
+})
+
 export const getModel = async (agent) => {
   switch (agent) {
     case "chat":
       return gpt;
     case "search":
       return compound;
+    case "coding":
+      return openRouter;
     default:
       return compound;
   };
