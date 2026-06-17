a45baa886a3ee7ca3a8be07c62a47e5a5076c429
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-17 10:01:08 +0530

    Title for artifacts is also returned from coding agent

--- services/agent/agents/coding.agent.js
+++ services/agent/agents/coding.agent.js
@@ -66,6 +66,7 @@ export const codingAgent = async (state) => {
         {
           id: Date.now(),
           type: "Project",
+          title: state?.prompt,
           files: content?.files || [],
         }
       ]
