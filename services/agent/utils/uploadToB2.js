55f952082c18f70206cba5bb896e42251bbce17f
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-20 11:35:29 +0530

    Created a util function to upload files to B2

---
+++ services/agent/utils/uploadToB2.js
@@ -0,0 +1,14 @@
+import { b2 } from "../config/b2.js";
+
+export const uploadToB2 = async (fileName, buffer, contentType) => {
+  await b2.send(
+    new PutObjectCommand({
+      Bucket: process.env.B2_BUCKET_NAME,
+      Key: fileName,
+      Body: buffer,
+      ContentType: contentType,
+    })
+  );
+
+  return fileName;
+}
