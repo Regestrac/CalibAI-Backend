d6ba658b3b728deeded49344bd3d3ad976d46634
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-20 10:32:42 +0530

    Created a util function to upload files to S3

---
+++ services/agent/utils/uploadToS3.js
@@ -0,0 +1,14 @@
+import { s3 } from "../config/s3.js";
+
+export const uploadToB2 = async (fileName, buffer, contentType) => {
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: fileName,
+      Body: buffer,
+      ContentType: contentType,
+    })
+  );
+
+  return fileName;
+}
