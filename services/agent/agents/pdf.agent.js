27501fbe9d7837c39375786a3633cd0d102adea2
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-21 16:46:10 +0530

    Implemented pdf agent

--- services/agent/agents/pdf.agent.js
+++ services/agent/agents/pdf.agent.js
@@ -1,3 +1,64 @@
-export const pdfAgent = async (params) => {
+import { getModel } from "../config/llmModels.js"
+import { generatePdf } from "../utils/generatePdf.js";
+import { getFromB2 } from "../utils/getFromB2.js";
+import { uploadToB2 } from "../utils/uploadToB2.js";
 
+export const pdfAgent = async (state) => {
+  try {
+    const llm = await getModel("pdf");
+
+    const prompt = `
+      You are an expert document writer.
+      Return ONLY valid JSON.
+      Do NOT return markdown.
+      Do NOT return explanations.
+
+      Structure:
+      {
+      "title":"".
+      "subtitle":,
+      "sections":[
+          {
+            "heading":"",
+            "points":[]
+          }
+        ]
+      }
+
+      Generate 4-8 sections.
+      Each section should have 3-6 concise bullet points.
+      Topic for content is based on user request.
+
+      User request: ${state?.prompt}
+    `;
+
+    const res = await llm.invoke(prompt);
+    const data = JSON.parse(res?.content);
+
+    const pdfBuffer = await generatePdf(data);
+
+    const fileName = `pdf-${Date.now()}.pdf`;
+
+    await uploadToB2(fileName, pdfBuffer, "application/pdf");
+    const downloadUrl = await getFromB2(fileName, 24 * 60);
+
+    return {
+      ...state,
+      aiResponse: `# 📄 PDF Generated Successfully
+      
+      **${data?.title}**
+      
+      You can download the PDF using the link below.
+      
+      🔗 [Download PDF](${downloadUrl})
+      
+      ⏳ _Link expires in 24 hours._`.replaceAll("\n      \n     ", "\n\n"),
+    }
+  } catch (error) {
+    console.log("PDF agent error: ", error);
+    return {
+      ...state,
+      aiResponse: "❌ Failed to generate PDF.",
+    }
+  }
 }
