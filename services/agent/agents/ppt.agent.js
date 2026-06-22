3ab328fc62375b6a54c0fe2321dad76664b5f575
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-22 10:50:17 +0530

    Implemented PPT agent

--- services/agent/agents/ppt.agent.js
+++ services/agent/agents/ppt.agent.js
@@ -1,3 +1,67 @@
-export const pptAgent = async (params) => {
+import { getModel } from "../config/llmModels.js"
+import { generatePpt } from "../utils/generatePpt.js";
+import { getFromB2 } from "../utils/getFromB2.js";
+import { uploadToB2 } from "../utils/uploadToB2.js";
 
+export const pptAgent = async (state) => {
+  try {
+    console.log('hit ppt agent: ',);
+    const llm = await getModel("ppt");
+
+    const prompt = `
+      You are a professional presentation designer.
+
+      Format:
+      {
+        "title":"",
+        "subtitle":"",
+        "slides":[
+          {
+            "title":"",
+            "points":["","",""]
+          }
+        ]
+      }
+
+      Rules:
+      - Generate exactly 6 content slides.
+      - Each slide should have 4-6 concise bullet points.
+      - No markdown.
+      - No explanation or reasoning.
+      - No code block.
+      - Return ONLY JSON.
+
+      Topic for content is based on user request.
+      User request: ${state?.prompt}
+    `;
+
+    const res = await llm.invoke(prompt);
+    const data = JSON.parse(res?.content);
+
+    const ppt = await generatePpt(data);
+    const buffer = await ppt.write({ outputType: "nodebuffer" });
+    const fileName = `ppt-${Date.now()}.pptx`;
+
+    await uploadToB2(fileName, buffer, "application/vnd.openxmlformats-officedocument.presentationml.presentation");
+    const downloadUrl = await getFromB2(fileName, 24 * 60 * 60);
+
+    return {
+      ...state,
+      aiResponse: `# 📄 PPT Generated Successfully
+      
+      **${data?.title}**
+      
+      You can download the PPT using the link below.
+      
+      🔗 [Download PPT](${downloadUrl})
+      
+      ⏳ _Link expires in 24 hours._`.replaceAll("\n      \n     ", "\n\n"),
+    }
+  } catch (error) {
+    console.log("PPT agent error: ", error)
+    return {
+      ...state,
+      aiResponse: '❌ Failed to generate PPT.'
+    }
+  }
 }
