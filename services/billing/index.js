a066528c32b84b2725a09ebd0fa26122c39a4cad
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-23 14:10:05 +0530

    Implemented the routes for the controllers in the billing service

--- services/billing/index.js
+++ services/billing/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dns from 'dns';
 import connectDB from './config/db.js';
+import router from './routes/billing.routes.js';
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from Billing" });
