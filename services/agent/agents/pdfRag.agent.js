489b43e8fb8631876115170a267a0e9106fa92ec
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-25 15:44:36 +0530

    Implemented pdf rag to analyze and response based on pdf file

--- services/agent/agents/pdfRag.agent.js
+++ services/agent/agents/pdfRag.agent.js
@@ -1,3 +1,66 @@
-export const pdfRagAgent = async (state) => {
+import fs from 'fs';
+import { PDFParse } from 'pdf-parse'
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { vectorStore } from '../config/vectorDb.js';
+import { getModel } from '../config/llmModels.js';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+import { deductCredits } from '../utils/deductCredits.js';
 
-}
+export const pdfRagAgent = async (state) => {
+  try {
+    const buffer = fs.readFileSync(state.file.path);
+
+    const pdf = new PDFParse({ data: buffer });
+
+    const result = await pdf.getText();
+    const text = result.text;
+
+    const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 });
+    const docs = await splitter.splitText(text);
+
+    const collectionName = `pdf-${Date.now()}`;
+
+    const store = await vectorStore(docs, collectionName);
+
+    const relevantDocs = await store.similaritySearch(state?.prompt, 5);
+
+    const context = relevantDocs.map((doc) => doc.pageContent).join("\n\n");
+
+    const llm = await getModel("pdfRag");
+
+    const messages = [
+      new SystemMessage(
+        `You are CalibAI PDF assistant.
+        
+        Rules:
+        - Answer ONLY for the uploaded PDF.
+        - Never make up information.
+        - If the answer is not present in the PDF, reply: "I couldn't find this information in the uploaded PDF".
+        - Use markdown formatting`
+      ),
+      new HumanMessage(
+        `Context: ${context}
+
+        Question: ${state.prompt}`
+      ),
+    ];
+
+    const response = llm.invoke(messages);
+
+    await deductCredits(state.userId, "pdfRag");
+
+    return {
+      ...state,
+      aiResponse: response?.content,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...state,
+      aiResponse: '❌ Failed to analyze PDF',
+    };
+  } finally {
+    fs.unlinkSync(state.file.path);
+  }
+};
