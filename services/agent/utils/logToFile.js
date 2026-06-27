5928331bd39f7a5638aa1f1132f8c37bd2e43aeb
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-27 16:32:11 +0530

    Improved log to file util function

--- services/agent/utils/logToFile.js
+++ services/agent/utils/logToFile.js
@@ -1,9 +1,23 @@
+import fs from 'fs';
+import path from 'path';
 
-const LOG_FILE = './debug.log';
+const LOG_FILE = './logs/debug.log';
 
 export const logToFile = (label, content, fileName = LOG_FILE) => {
+  const dir = path.dirname(fileName);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
   const timestamp = new Date().toLocaleString();
   const separator = '='.repeat(30);
-  const entry = `\n${separator} [${timestamp}] ${label} ${separator}\n${content}\n`;
+  const seen = new WeakSet();
+  const contentString = JSON.stringify(content, (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) return `[Circular]: ${key}`;
+      seen.add(value);
+    }
+    return value;
+  }, 2)
+  const entry = `\n${separator} [${timestamp}] ${label} ${separator}\n${contentString}\n`;
   fs.appendFileSync(fileName, entry);
 };
