23b9146a23f31c3672df1e1dccdef723d984e3d8
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-30 15:36:16 +0530

    Updated firebase initialize app method

--- services/auth/config/firebase.js
+++ services/auth/config/firebase.js
@@ -1,6 +1,9 @@
 import { cert, initializeApp } from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
 
 export const app = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert({
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  })
 });
